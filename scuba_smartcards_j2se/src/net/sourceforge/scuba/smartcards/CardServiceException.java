143ac22576e333c613b7a958e3fc10cd23e14258
martijno <martijno@b350d931-f925-4dad-8688-0e260e346352>
2009-03-25 10:56:12 +0000

    git-svn-id: https://scuba.svn.sourceforge.net/svnroot/scuba@18 b350d931-f925-4dad-8688-0e260e346352

---
+++ scuba_smartcards_j2se/src/net/sourceforge/scuba/smartcards/CardServiceException.java
@@ -0,0 +1,10 @@
+package net.sourceforge.scuba.smartcards;
+
+public class CardServiceException extends Exception
+{
+	private static final long serialVersionUID = 4489156194716970879L;
+
+	public CardServiceException(String msg) {
+		super(msg);
+	}
+}
---
+++ scuba_smartcards_j2se/src/net/sourceforge/scuba/smartcards/CardTerminalListener.java
@@ -0,0 +1,27 @@
+package net.sourceforge.scuba.smartcards;
+
+import java.util.EventListener;
+
+/**
+ * Interface for card insertion and removal event observers.
+ *
+ * @author Martijn Oostdijk (martijn.oostdijk@gmail.com)
+ *
+ * @version $Revision: $
+ */
+public interface CardTerminalListener extends EventListener
+{
+	/**
+	 * Called when card inserted.
+	 *
+	 * @param ce insertion event
+	 */
+	void cardInserted(CardEvent ce);
+
+	/**
+	 * Called when card removed.
+	 *
+	 * @param ce removal event
+	 */
+	void cardRemoved(CardEvent ce);
+}
