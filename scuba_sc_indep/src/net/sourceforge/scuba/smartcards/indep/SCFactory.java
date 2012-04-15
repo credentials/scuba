512725c92d5f852115406899d153ea416fba359a
martijno <martijno@b350d931-f925-4dad-8688-0e260e346352>
2012-04-15 12:37:49 +0000

    git-svn-id: https://scuba.svn.sourceforge.net/svnroot/scuba@180 b350d931-f925-4dad-8688-0e260e346352

--- scuba_sc_indep/src/net/sourceforge/scuba/smartcards/indep/SCFactory.java
+++ scuba_sc_indep/src/net/sourceforge/scuba/smartcards/indep/SCFactory.java
@@ -6,34 +6,27 @@ import net.sourceforge.scuba.smartcards.ISCFactory;
 
 public class SCFactory implements ISCFactory<CommandAPDU, ResponseAPDU> {
 
-	@Override
 	public ICommandAPDU createAccessorC(CommandAPDU c) {
 		return c;
 	}
 
-	@Override
 	public IResponseAPDU createAccessorR(ResponseAPDU r) {
 		return r;
 	}
 
-	@Override
 	public CommandAPDU createCommandAPDU(int cla, int ins, int p1, int p2, byte[] data, int ne) {
 		return new CommandAPDU(cla, ins, p1, p2, data, ne);
 	}
 
-	@Override
 	public CommandAPDU createCommandAPDU(int cla, int ins, int p1, int p2, byte[] data) {
 		return new CommandAPDU(cla, ins, p1, p2, data);
 	}
 
-	@Override
 	public CommandAPDU createCommandAPDU(int cla, int ins, int p1, int p2, int ne) {
 		return new CommandAPDU(cla,ins, p1, p2, ne);
 	}
 
-	@Override
 	public ResponseAPDU createResponseAPDU(byte[] bytes) {
 		return new ResponseAPDU(bytes);
 	}
-
 }
